d31f5bdeeb63885ba0004b7bb67979a14abb9e1b
tameribrahim <tameribrahim@dhcp-199-207.eduroam.carleton.ca>
2018-09-14 09:13:23 -0400

    The changes I made for AddressBook class.

---
+++ Lab1/src/AddressBook.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+	private ArrayList<BuddyInfo> buddyInfo;
+	
+	public void addBuddy(BuddyInfo aBuddyInfo) {
+		if(aBuddyInfo!=null) {
+			buddyInfo.add(aBuddyInfo);
+			
+		}
+	}
+	public void removeBuddy(int index) {
+		if(index >= 0 && index < buddyInfo.size()) {
+			buddyInfo.remove(index);
+			
+		}
+	}
+		public static void main(String[]args) {
+			System.out.println("Address Book");
+		}
+	}
