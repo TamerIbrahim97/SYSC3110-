bb2a720cc5d753f518ab4a240bc3b24482c9b901
tameribrahim <tameribrahim@dhcp-199-207.eduroam.carleton.ca>
2018-09-14 09:10:36 -0400

    The changes done for my AddressBook class

---
+++ Lab1/src/BuddyInfo.java
@@ -0,0 +1,50 @@
+
+public class BuddyInfo {
+	private String name;
+	private String address;
+	private int phoneNumber;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BuddyInfo Tamer = new BuddyInfo();
+		System.out.println("Hello World!");
+		System.out.println(Tamer.getName());
+	}
+
+	public String getName() {
+		name= "Tamer";
+		return name;
+		
+	}
+
+	public void setName(String Tamer) {
+		this.name = Tamer;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+public class AddressBook {
+	public void addBuddy() {
+	}
+	public void removeBuddy() {
+		
+	}
+	}
+
+}
+
+
